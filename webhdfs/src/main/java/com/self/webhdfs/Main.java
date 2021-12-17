55ded67b0842b942f29154d80a4d075ac5ffcd34
guhabiswasavijit <guhabiswas.avijit@gmail.com>
2021-12-17 12:13:55 +0530

    read write to hdfs

--- webhdfs/src/main/java/com/self/webhdfs/Main.java
+++ webhdfs/src/main/java/com/self/webhdfs/Main.java
@@ -1,32 +1,42 @@
 package com.self.webhdfs;
 
-import org.apache.hadoop.fs.FileStatus;
+import java.net.URI;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.AbstractFileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.hadoop.fs.WebHdfs;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-@ImportResource("hadoop-context.xml")
 @SpringBootApplication
 public class Main implements CommandLineRunner {
 
-    @Autowired
-    private FileSystem fs;
+	private static final String NAME_NODE = "hdfs://webhdfs:50020";
 
     @Override
     public void run(String... strings) throws Exception {
-        Path[] paths = new Path[strings.length];
-        for (int i = 0; i < strings.length; ++i) {
-            paths[i] = new Path(strings[i]);
-        }
-        for (FileStatus stat: fs.listStatus(paths)) {
-            System.out.println(stat.getPath());
-        }
+    	Configuration conf = new Configuration();
+    	Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", NAME_NODE);
+        configuration.set("fs.hdfs.impl",org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        configuration.set("fs.file.impl",org.apache.hadoop.fs.LocalFileSystem.class.getName());
+        FileSystem hdfsFileSystem = FileSystem.get(configuration);
+        Path writeFile = new Path("/geeks/newFile.txt");
+        hdfsFileSystem.create(writeFile);
+        FileUtil.write(hdfsFileSystem,writeFile,"I'm new to hdfs".getBytes());
+        Path readPath = new Path("/geeks/newToHdfs.txt");
+        AbstractFileSystem webhdfs = WebHdfs.get(new URI("http://webhdfs:50070"),conf);
+        FSDataInputStream inputStream = webhdfs.open(readPath);
+        String out= IOUtils.toString(inputStream, "UTF-8");
+        System.out.println(out);
+        
+
     }
 
     public static void main(String[] args) {
